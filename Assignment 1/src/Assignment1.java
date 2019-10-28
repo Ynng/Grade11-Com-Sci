d97d0f5c5985efe8a19a869a1aae8e9a175f785a
Ynng <44907675+Ynng@users.noreply.github.com>
2019-10-28 01:21:13 -0400

    commented everything yay

--- Assignment 1/src/Assignment1.java
+++ Assignment 1/src/Assignment1.java
@@ -184,7 +184,7 @@ public class Assignment1 {//Start of Assignment1 Class
             printHorizontalLine(); //prints a horizontal line to seperate the screen visualy, so it looks better
         }//end of main inifinite loop
         s.close();//closes the scanner to prevent resource leak at the very end of the program
-    }
+    }//end of main method
 
     private static void printCorrectPercentage(int correct, int total) {//start of printCorrectPercentage function, which takes in the correct number of questions the user has answered and the total number of questions the user has answered and prints out the percentage of correct answers the user has given
         System.out.println("Your total percentage of correct answers is " + makePercentageString(correct, total));//user friendly output
@@ -201,100 +201,101 @@ public class Assignment1 {//Start of Assignment1 Class
             System.out.println("Or, enter " + makeBoldString(keyWord) + " " + explanation);//user friendly starting with "or, enter", with the keyword bolded and the explanation after it
     }//end of printMenuOption function
     
-    private static void printChoiceNotice(String action, String keyWord) {
+    private static void printChoiceNotice(String action, String keyWord) {//start of printChoiceNotice function, which takes in the keyword the user has entered, and a description of the action the user has caused
         printEraseScreen(); //erases the screen, so the user gets a fresh new screen with no previous text making it look messy.inuing
         printHorizontalLine(); //prints a horizontal line to seperate the screen visualy, so it looks better
-        System.out.println("You have " + action + " by entering " + makeBoldString(keyWord));
-    }
+        System.out.println("You have " + action + " by entering " + makeBoldString(keyWord));//user friendly output telling the user about what the user has entered that triggered the action, and what action the program is going to execute
+    }//end of printChoiceNotice function
 
-    private static void printHorizontalLine() {
-        System.out.println("__________________________________________________________");
-    }
+    private static void printHorizontalLine() {//start of printHorizontalLine function, which prints a horizontal line, taking in no input and returns nothing, therefore void
+        System.out.println("__________________________________________________________"); //print a horizontal line to the screen, so the console will look nicer
+    }//end of printHorizontalLine function
 
-    private static String makePercentageString(int a, int b) {
-        return Integer.toString((int) (Math.round((float) a / (float) b * 100))) + "%";
-    }
+    private static String makePercentageString(int a, int b) {//start of makePercentageString function, which takes in 2 numbers, finds the percentage by dividing the two numbesr and prints it out in a neet way
+        return Integer.toString((int) (Math.round((float) a / (float) b * 100))) + "%"; //returns the percentage made by dividing the two numbers, with the percentage sign behind it
+    }//end of makePercentageString function
 
-    private static String makeBoldString(String text) {
-        return ANSI_BOLD + text + ANSI_RESET;
-    }
+    private static String makeBoldString(String text) {//start of makeBoldString function, which takes in a string, and returns that same string, with the ansi escape characters that makes it bold before it, and the ansi escape characters that resets styling afterwards
+        return ANSI_BOLD + text + ANSI_RESET; //returns the string passed in but with the ansi escape characters that makes it bold before it, and the ansi escape characters that resets styling afterwards
+    }//end of makeBoldString function
 
-    private static int getIntGuranteed(Scanner s, int lowerLimit, int upperLimit) {
-        int output = -1;
+    private static int getIntGuranteed(Scanner s, int lowerLimit, int upperLimit) {//start of getIntGuranteed function, which takes in the scanner, a lower limit and an upper limit. It handles all the try catch, error proofing and user prompting for getting an integer from the user within a certain range. Therefore, int *guranteed*, because all the errors are handled by itself and won't be thrown out to the main method
+        int output = lowerLimit-1;//initalizes and declares an integer called output to 1 lower then the lower limit, so that even if the user didn't enter an integer and an error occurred, "output" would've still be initialized and won't cause any additional errors
 
-        while (true) {
-            try {
-                output = s.nextInt();
-            } catch (java.util.InputMismatchException e) {
-                s.nextLine();
-            }
-            if (output >= lowerLimit && output <= upperLimit) {
-                return output;
-            } else {
-                System.out.print("That was an invalid input, please enter a single integer between 1 and " + upperLimit
-                        + ", such as \"" + lowerLimit + "\" or \"" + upperLimit + "\"Try again now: ");
-            }
-        }
-    }
+        while (true) {//start of an inifinite loop that the user has to enter an acceptable integer to get out from
+            try {//start of try catch block
+                output = s.nextInt();//tries to get an integer from the user, user friendly output should be printed before calling "getIntGuranteed"
+            } catch (java.util.InputMismatchException e) {//catches the input mismatch exception, which nextInt() might throw if the didn't enter an integer'
+                s.nextLine();//throws away the invalid input by reading it with nextLine, which gurantees nothing (most of the time) can go wrong 
+            }//end of try catch block
+            if (output >= lowerLimit && output <= upperLimit) {//start of if statement, checks if the integer is within the range of acceptable inputs
+                return output;//if the integer is within the range of acceptable inputs, return it
+            } else {//if the integer is outside the range of acceptable inputs
+                System.out.print("That was an invalid input, please enter a single integer between 1 and " + upperLimit + ", such as \"" + lowerLimit + "\" or \"" + upperLimit + "\"Try again now: ");//user friendly output retelling the user about the range of acceptable inputs, and prompts the user to try again
+            }//end of if statement
+        }// end of while loop
+    }//end of function getIntGuranteed
 
-    private static int generateQuestion(int overallOperationLimit, int maxTermLength) {
+    private static int generateQuestion(int overallOperationLimit, int maxTermLength) {//start of generateQuestion function, which is the center part of the program. It generates a random equation, along with its answer and prints it to the console for the user to answer
         // generating Question
-        int termAnswer, termPrevious, termOperationLimit, nextDigit = 0, operationCounter = -1, overallAnswer = 0;
-        char overallOperation = 'i', termOperation = 'i'; // i for initial
-        String question = "";
-        Random ran = new Random();
+        //term: one term of a polynomial, made up of only multiplication and/or division
+        int termAnswer,//declares an integer variable called termAnswer, which helps the program to temporarily store the answer of a term while generating it sequentially
+            termPrevious,//declares an integer variable called termPrevious, which helps the program to temporarily store the "previous" answer of a term while generating it
+            termOperationLimit,//declares an integer variable called termOperationLimit, which helps the program to temporarily store the number of operations the program chose to have in a given term
+            nextDigit = 0,//declares and initializes an integer variable called nextDigit to 0, which helps the program to temporarily store the next digit of a term while generating it, so the program can multiply/divide it by the "previous" answer and finds the overall answer. It is initialized to 0 to prevent a "variable might not be initialized" error, as the variable is only initialized in a switch statement without a default case, powered by a random number generator. Despite being logically impossible that the statement using this variable would have run without the variable being initialized, the compiler doesnt know that, therefore I'll just initialize it here to some random value, I chose 0, but it doesnt actually matter 
+            operationCounter = -1, //declares an integer variable called operationCounter, which helps the program to cound the number of operations in the equation, it is limited to the level number, so that the equation can get progressively harder 
+            overallAnswer = 0; //declares and initialize an integer variable called overallAnswer, which helps the program to store the overall answer of the equation while generating it.
+        char overallOperation = 'i', termOperation = 'i'; // declares and initialize the character variable called overallOperation and termOperation, which temporarily stores the sign of the operation, so that the program can run a switch statement on it and figure out what operation it needs to perform between the "previous" number/term and the "next"number/term to find the overall/term answer 
+        String question = "";//declares and initialize a string variable called question, which stores the quetsion, with the digits and operators in string format, for printing to the console for the user to read 
+        Random ran = new Random();//declares and initializes a Random object called ran, which is the main way the program randomizes its equation
 
-        do {
-            termAnswer = termPrevious = ran.nextInt(10);
-            if (overallOperation == 'i')
-                overallOperation = '+';
-            else
-                question += " " + overallOperation;
-            question += " " + termPrevious;
-            operationCounter++;
+        do {//start of main loop, which generates a term and adds it to the rest of the equation, which will be generated from the previous iterations of the loop
+            termAnswer = termPrevious = ran.nextInt(10);//randomly generates the first digit of the term and sets the value of both termAnswer and termPrevious to it.
+            if (overallOperation == 'i')//start of if statement, checks if this is the first digit of the equation
+                overallOperation = '+';//if so, set overallOperation to + and don't put it in the string question, which will be printed to the user. Because despite technically, the program is going through 0 + [the first digit] and therefore wants to print that, this simply doesnt make sense, simply [the first digit] is enough.
+            else//if this is not the first digit of the equation, therefore there are numbesr and equations in the string question before this already
+                question += " " + overallOperation;//add a space followed by the equation, which will be generated at the end of the loop 
+            question += " " + termPrevious;//adds a space followed by the first digit of the term
+            operationCounter++;//since we just added a digit and an operation to our quetsion, increment the operation counter 
 
-            termOperationLimit = ran.nextInt(Math.min(overallOperationLimit - operationCounter, maxTermLength) + 1);
-            for (int termOperationCount = 0; termOperationCount < termOperationLimit; termOperationCount++) {
-                switch (ran.nextInt(2)) { // randomly choose between multiplication and subtraction, therefore a
-                                          // single term in a polynomial
-                case 0:// multiplication
-                    nextDigit = ran.nextInt(10); // generates the next digit in the question
-                    termOperation = '×';
-                    termAnswer = termPrevious * nextDigit;
-                    break;
-                case 1:// division
-                    termOperation = '÷';
-                    do {
-                        do {
-                            nextDigit = ran.nextInt(10); // generates the next digit in the question
-                        } while (nextDigit == 0); // to make sure the next digit is not 0 because
-                        termAnswer = termPrevious / nextDigit;
-                    } while (termAnswer * nextDigit != termPrevious);
-                    break;
-                }
-                termPrevious = termAnswer;
-                question += " " + termOperation + " " + nextDigit;
-                operationCounter++;
-            }
-            switch (overallOperation) {
-            case '+':
-                overallAnswer += termAnswer;
-                break;
-            case '-':
-                overallAnswer -= termAnswer;
-                break;
-            }
-            switch (ran.nextInt(2)) { // randomly choose between addition or subtraction
-            case 0:// addition
-                overallOperation = '+';
-                break;
-            case 1:// subtraction
-                overallOperation = '-';
-                break;
-            }
-        } while (operationCounter < overallOperationLimit);
-        // System.out.println(question += " = " + overallAnswer);
-        System.out.print(question += " = ");
-        return overallAnswer;
-    }
-}
+            termOperationLimit = ran.nextInt(Math.min(overallOperationLimit - operationCounter, maxTermLength) + 1);//randomly generates the number of operations our current term will have, the number is limited by the remaining number of operations our question can still have and the max number of operations our term can have, it is incremented by 1 because the Random class's nextInt method outputs maximum ("your input" -1), when we actually want it to output maximum ("your input"), so we increment our input to the nextInt method by 1 to cancel it out and achieve our desired range of random numbesr
+            for (int termOperationCount = 0; termOperationCount < termOperationLimit; termOperationCount++) {//start of for loop that generates a term in our polynomial equation, each iteration through this loop will add an operation and a digit to our quetsion  
+                switch (ran.nextInt(2)) { // start of switch statement that randomly choose between 0 and 1, which mathces up to multiplication and subtraction
+                case 0:// if 0 is chosen
+                    nextDigit = ran.nextInt(10); // sets the value of nextDigit to a random digit, from 0 to 9.
+                    termOperation = '×'; //sets the value of termOperation to the multiplication sign, which will be added to the quetsion string, which will only be used as a user friendly prompt  
+                    termAnswer = termPrevious * nextDigit; //finds the total value of the term, as it currently is, so it can be accumulated through the loop and so we can arrive at the true total value of the term later on
+                    break;//end of the case if 0 is chosen, or, multiplication is chosen
+                case 1:// if 1 is chosen by the random number generator, therefore we go with division
+                    termOperation = '÷';//sets the value of termOperation to the division sign, which will be added to the quetsion string, which will only be used as a user friendly prompt
+                    do {//start of do while loop that doesnt stop until the answer of the term is an integer, which is what we want. Our quetsions shouldnt involve any digits at all in any step
+                        nextDigit = ran.nextInt(9)+1; // sets the value of nextDigit to a random digit, from 1 to 9, the random number generator will generate an integer from 0 to 8, and then it will be incremented by 1, to 1 to 9. This digit can not be 0 as divisionby 0 is impossible 
+                        termAnswer = termPrevious / nextDigit;// finds the total value of the term, as it currently is, so it can be accumulated through the loop and so we can arrive at the true total value of the term later on 
+                    } while (termAnswer * nextDigit != termPrevious);//end and condition statement of the do while loop. The loop doesnt end until the answer of the term is an integer
+                    break;//end of the case if 1 is chosen, or, division is chosen 
+                }//end of random term and digit generation
+                termPrevious = termAnswer;//sets the value of termPrevious to the current term answer, as the current total value of the term will be the "previous" value of the term in the next iteration of the for loop  
+                question += " " + termOperation + " " + nextDigit;//adds a space followed by the new digit we just generated  
+                operationCounter++;//since we just added a digit and an operation to our questioe, increment the operation counter   
+            }//end of term generation loop
+            switch (overallOperation) {//start of a switch block that either add or divide the total value of the term to the total value of the whole equation, depending on the operation generated at the end of the loop in the last iteration, or "+" set at the start manually if this is the first iteration 
+            case '+'://if the value of overallOperation is +
+                overallAnswer += termAnswer;//adds the total value of the term to the total value of the whole equation
+                break;//end of the case if the value of overallOperation is +
+            case '-'://if the value of overallOperation is -
+                overallAnswer -= termAnswer;//subtracts the total value of the term from the total value of the whole equation
+                break;//end of the case if the value of overallOperation is -
+            }//end of the switch block that either add or divide the total value of the term to the total value of the whole equation
+            switch (ran.nextInt(2)) { //start of a switch block that either randomly decides the value of overallOperation, it is powerd by a random number generator that outputs either 0 or 1
+            case 0:// if the output of the random number generator is 0, we do addition 
+                overallOperation = '+';//sets the value of overallOperation to the plus sign
+                break;//end of the case if the output of the random number generator is 0
+            case 1:// if the output of the random number generator is 1, we do subtraction
+                overallOperation = '-';//sets the value of overallOperation to the minus sign
+                break;//end of the case if the output of the random number generator is 1
+            }//end of the switch block that decides the value of overallOperation
+        } while (operationCounter < overallOperationLimit);//end of the main loop that genearte and adds/subtracts the term from the equation.
+        System.out.print(question += " = ");//prints the string quetsion, which is filled up with a mathematical expression in the while loop above, followed by an equal sign surounded by spaces, to prompt the user to answer the question   
+        return overallAnswer;//returns the answer, which is the total value of the whole expression, so the logic in the main method can handle user input and determine if the user got the quetsion right
+    }//end of the generateQuestion method, a quetsion has been geneated and printed, an answer has been returned
+}//end of the Assignment1 class! yay I finally finished commenting all this
