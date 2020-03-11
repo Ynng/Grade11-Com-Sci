c80354ce96bb5fc0243bd19d61abb227675597ce
Ynng <44907675+Ynng@users.noreply.github.com>
2020-03-11 14:57:31 -0400

    ajsdl;fksadfasdf

--- Gr12 Assignment 1/src/app/Solver.java
+++ Gr12 Assignment 1/src/app/Solver.java
@@ -10,28 +10,7 @@ public class Solver {
   public boolean solveGrid() {
     visited = new Grid(grid.getSize());
     visited.write(new Coordinate(1, 1), Grid.VISITED);
-    boolean output = recurs(new Coordinate(1, 1));
-    printResult();
-    return output;
-  }
-
-  private void printResult() {
-    final String ANSI_NOT_REACHED = "\u001B[31m";
-    final String ANSI_RESET = "\u001B[0m";
-    Coordinate coord;
-    for (int row = 1; row <= grid.getSize(); row++) {
-      for (int col = 1; col <= grid.getSize(); col++) {
-        coord = new Coordinate(row, col);
-        if (visited.read(coord) != Grid.VISITED) {
-          System.out.print(ANSI_NOT_REACHED);
-        }
-        System.out.printf("%-3s", grid.read(coord));
-        if (visited.read(coord) != Grid.VISITED) {
-          System.out.print(ANSI_RESET);
-        }
-      }
-      System.out.println();
-    }
+    return recurs(new Coordinate(1, 1));
   }
 
   private boolean recurs(Coordinate coord) {
