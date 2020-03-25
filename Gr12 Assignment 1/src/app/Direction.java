78416be738e2b7ef1a6e061c7f6b1e5ecacc5ac6
Ynng <44907675+Ynng@users.noreply.github.com>
2020-03-24 20:35:48 -0400

    commented direction

--- Gr12 Assignment 1/src/app/Direction.java
+++ Gr12 Assignment 1/src/app/Direction.java
@@ -1,8 +1,30 @@
 package app;
+//****************************************************************************************
+//Coordinate Class
+//Student Name
+//Date: 
+//Programming language, IDE,  and version (example java, Eclipse 3.1.6)
+//****************************************************************************************
 
+/** Directions that can be used */
 public enum Direction {
+  /**
+   * Left direction
+   */
   LEFT,
+
+  /**
+   * Right direction
+   */
   RIGHT,
+
+  /**
+   * Up direction
+   */
   UP,
+
+  /**
+   * Down direction
+   */
   DOWN;
 }
