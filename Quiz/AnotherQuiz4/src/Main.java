0a36113b65b31cf4dc5e0d4cd09c4fbe43d42285
Ynng <44907675+Ynng@users.noreply.github.com>
2020-06-09 19:53:04 -0400

    minor

--- Quiz/AnotherQuiz4/src/Main.java
+++ Quiz/AnotherQuiz4/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    LargeAnt ant = new LargeAnt(10);
-    System.out.println(LargeAnt.getAntCounter());
+    Ant ant = new LargeAnt(10);
+    System.out.println(ant.getClass().getSimpleName());
   }
 }
